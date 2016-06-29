57418b1556744e6a8919ab43fb0e989b54645caa
thanksdanny <hj-07@163.com>
2016-06-30 00:26:45 +0800

    Initial Commit

---
+++ GuZhi_V2EX/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GuZhi_V2EX
+//
+//  Created by Danny Ho on 6/30/16.
+//  Copyright © 2016 thanksdanny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
