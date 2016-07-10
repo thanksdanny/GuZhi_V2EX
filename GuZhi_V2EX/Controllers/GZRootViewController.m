63766e0461072dfe6295da4603310c754be2d5fb
thanksdanny <hj-07@163.com>
2016-07-10 18:17:57 +0800

    json解析

--- GuZhi_V2EX/Controllers/GZRootViewController.m
+++ GuZhi_V2EX/Controllers/GZRootViewController.m
@@ -12,8 +12,8 @@
 @interface GZRootViewController ()
 
 @property (nonatomic, strong) NSArray *hotList;
+@property (nonatomic)  NSMutableArray *hotTitle;
 @property NSURLSession *session;
-@property NSMutableDictionary *hotTitle;
 
 @end
 
@@ -36,6 +36,15 @@
     self.title = @"最新";
 }
 
+# pragma mark - 懒加载
+
+- (NSArray *)hotTitle {
+    if (!_hotTitle) {
+        _hotTitle = [NSMutableArray array];
+    }
+    return _hotTitle;
+}
+
 # pragma mark - 获取网络数据
 
 // 获取hot https://www.v2ex.com/api/topics/hot.json
@@ -43,14 +52,17 @@
     NSURL *hotURL = [NSURL URLWithString:@"https://www.v2ex.com/api/topics/hot.json"];
     
     NSURLSessionDataTask *dataTask = [self.session dataTaskWithURL:hotURL completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
-        NSArray *json = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:nil];
+        // 解析json
+        NSArray *json = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
         self.hotList = json;
-        self.hotTitle = [NSMutableDictionary dictionary];
-        for (int i = 0; i < self.hotList.count; i++) {
-//            [self.hotTitle setObject:self.hotList[i] forKey:@"title"];
-            NSLog(@"%@", self.hotList);
-        }
         
+//        for (int i = 0; i < self.hotList.count; i++) {
+//            NSString *title = [self.hotList[i] objectForKey:@"title"];
+////            NSLog(@"%@", title);
+//            [self.hotTitle addObject:title];
+//        }
+        
+        NSLog(@"%@", self.hotTitle);
     }];
     
     
@@ -78,48 +90,6 @@
     return cell;
 }
 
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
 
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
