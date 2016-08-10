8d397af9df9acb96e9e1021ef27b6680bf255965
thanksdanny <hj-07@163.com>
2016-08-10 23:25:02 +0800

    调整首页ui

--- GuZhi_V2EX/Views/GZTopicListCell.m
+++ GuZhi_V2EX/Views/GZTopicListCell.m
@@ -15,32 +15,18 @@
 
 @implementation GZTopicListCell
 
-// 貌似这方法没用 进不去用sb的话
-- (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
-    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
-    NSLog(@"haha");
-    if (self) {
-        self.clipsToBounds = YES;
-        self.selectionStyle = UITableViewCellSelectionStyleNone;
-        
-        [self configureViews];
-    }
+- (void)awakeFromNib {
+    [super awakeFromNib];
     
-    return self;
-}
-
-
-#pragma mark - Congifure Views
-
-// 手写界面的时候在这配置ui
-- (void)configureViews {
-    NSLog(@"heihei");
 }
 
 #pragma mark - Data Methods
 
 - (void)setModel:(GZHotModel *)model {
     _model = model;
+    
+    self.clipsToBounds = YES;
+    self.selectionStyle = UITableViewCellSelectionStyleNone;
     
     self.titleLable.text = model.title;
     
